16fcfec80459ec0f0b1e5a7d507b6a20cc76fbe9
Gesine Cauer <gesine.cauer@gmail.com>
2025-07-16 19:37:21 -0700

    reorganizing code

---
+++ jupyter/fit_dist_decay.py
@@ -0,0 +1,206 @@
+import pandas as pd
+import glob
+import os
+import ast
+import numpy as np
+from cooltools.sandbox.expected_smoothing import log_smooth
+
+import matplotlib.pyplot as plt
+from matplotlib import colors
+# %matplotlib inline
+plt.style.use('seaborn-v0_8-poster')
+import seaborn as sns
+
+import jax.numpy as jnp
+from jax import grad
+from scipy import optimize
+from estimate_alpha import print_alpha_infer_results
+
+
+
+# ===================================================================================================================
+# ===================================================================================================================
+# ===================================================================================================================
+
+def agg_across_genomic_dist(df, min_n=None):
+    data_sum = df.sum(axis=0)
+    data_sum.name = 'data'
+    n_detected_sum = df.notnull().sum(axis=0)
+    n_detected_sum.name = 'n'
+    if min_n is not None and min_n > 1:
+        nan_mask = n_detected_sum < min_n
+        data_sum[nan_mask] = 0
+        n_detected_sum[nan_mask] = 0
+    res = pd.concat([
+        data_sum.to_frame().T, n_detected_sum.to_frame().T])
+    return res
+
+
+def get_genomic_dist_by_cell(sc_dis_intramol, min_n=None, dis_exp=-1):
+    sc_invdis_intramol = sc_dis_intramol.pow(dis_exp)
+    sc_invdis_intramol.index = [j - i for i, j in sc_invdis_intramol.index]
+    sc_invdis_intramol.columns.name = 'cell_num'
+    sc_invdis_intramol.sort_index(inplace=True)
+    s_by_cell = sc_invdis_intramol.groupby(level=0).apply(agg_across_genomic_dist, min_n=min_n)
+    s_by_cell.index.names = ['genomic_dis', 'desc']
+    return s_by_cell
+
+
+def smooth_frequency(df_freq, sigma_log10=0.1):
+    df_freq_smooth = []
+    
+    for col in df_freq.columns:
+        cell_data = df_freq[col].unstack()
+        cell_data.columns.name = None
+
+        mask = cell_data.n > 0
+        if sigma_log10 is None or not sigma_log10:
+            cell_data['smoothed_data'] = cell_data.data
+            cell_data['smoothed_n'] = cell_data.n
+        else:
+            smoothed_data, smoothed_n = log_smooth(
+                cell_data[mask].index.values.astype(np.float64),
+                cell_data.loc[mask, ['data', 'n']].values.T,
+                sigma_log10=sigma_log10,
+                window_sigma=5, points_per_sigma=10)
+            cell_data.loc[mask, 'smoothed_data'] = smoothed_data
+            cell_data.loc[mask, 'smoothed_n'] = smoothed_n
+
+        cell_data['smoothed_freq'] = np.nan
+        mask_sm = mask & cell_data.smoothed_n != 0
+        cell_data.loc[mask_sm, 'smoothed_freq'] = cell_data.loc[mask_sm, 'smoothed_data'] / cell_data.loc[mask_sm, 'smoothed_n']
+        cell_data['cell_num'] = col
+        cell_data['n'] = cell_data['n'].astype(int)
+    
+        df_freq_smooth.append(cell_data)
+    
+    df_freq_smooth = pd.concat(df_freq_smooth)
+    return df_freq_smooth
+
+
+def estimate_exponent_manual(s_by_cell_smooth_agg, x0=None, bounds=(-10, 10), seed=0,
+                             min_genomic_dis=None, max_genomic_dis='default',
+                             max_iter=1e20, resolution_bp=2.5e6, verbose=True, plot=True, dis_exp=-1):
+
+    dist_decay = s_by_cell_smooth_agg.smoothed_freq.copy()
+    dist_decay.index *= int(resolution_bp)
+    
+    # Build args
+    if min_genomic_dis is None:
+        min_genomic_dis = dist_decay.index.min()
+    if max_genomic_dis is None:
+        max_genomic_dis = dist_decay.index.max()
+    elif isinstance(max_genomic_dis, str) and max_genomic_dis.lower() == 'default':
+        max_genomic_dis = 6e7
+    mask = (dist_decay.index <= max_genomic_dis) & (dist_decay.index >= min_genomic_dis)
+    genomic_dis = dist_decay[mask].index.values
+    freq_smooth = dist_decay[mask].values.ravel()
+
+    d = estimate_exponent(
+        genomic_dis, freq_smooth=freq_smooth, x0=x0, bounds=bounds, seed=seed,
+        max_iter=max_iter, verbose=verbose - 1)
+
+    x = genomic_dis
+    y = d['scaling_factor'] * np.power(x, d['exponent'])
+
+    exponent_dis3d = d['exponent'] / dis_exp
+    est_alpha = -0.8 / exponent_dis3d
+    if verbose:
+        if verbose > 1:
+            print(flush=True)
+        print(f"v = {exponent_dis3d:.3g}")
+        print(f"1/v = {1 / exponent_dis3d:.3g}")
+        print(f"est alpha = B/v = {est_alpha:.3g}")
+
+    if plot:
+        f, ax = plt.subplots(figsize=[6.4, 4.8])
+        ax.loglog(
+            s_by_cell_smooth_agg.index.values * 2.5e6,
+            s_by_cell_smooth_agg.smoothed_freq.values,
+            label='MERFISH data')
+        ax.set(
+            xlabel='Separation, bp ($s$)', 
+            ylabel='Inverse of 3D\n' + r'distances $(d^{' + f"{dis_exp:g}" '})$')
+        ax.set_aspect(1.0)
+        ax.grid(lw=0.5)
+        
+        ax.loglog(x, y, color='black', label=r"$d \sim s^{" + f"{exponent_dis3d:.3g}" + r"}$")
+
+        # ax.set_ylim()
+        ax.set_title(f"")
+        plt.legend()
+        f.tight_layout()
+        plt.show()
+
+    return d, (x, y), est_alpha
+
+# ===================================================================================================================
+# ===================================================================================================================
+# ===================================================================================================================
+
+def estimate_scaling_factor(exponent, genomic_dis, freq_smooth):
+    y = jnp.power(genomic_dis, exponent)
+    scaling_factor = jnp.sum(y * freq_smooth) / jnp.sum(jnp.square(y))
+    return scaling_factor
+
+
+def fit_exponent_obj(exponent, genomic_dis, freq_smooth, scaling_factor):
+    y = scaling_factor * jnp.power(genomic_dis, exponent)
+    mse = jnp.mean(jnp.square(freq_smooth - y))
+    return mse
+
+
+fit_exponent_grad = grad(fit_exponent_obj)
+
+
+def obj_wrap(exponent, genomic_dis, freq_smooth):
+    scaling_factor = estimate_scaling_factor(exponent, genomic_dis=genomic_dis, freq_smooth=freq_smooth)
+    obj = fit_exponent_obj(exponent, genomic_dis=genomic_dis, freq_smooth=freq_smooth, scaling_factor=scaling_factor)
+    if not isinstance(obj, float) and obj.size > 1:
+        obj = sum(obj)
+    return obj
+
+
+def grad_wrap(exponent, genomic_dis, freq_smooth):
+    scaling_factor = estimate_scaling_factor(exponent, genomic_dis=genomic_dis, freq_smooth=freq_smooth)
+    return np.array(fit_exponent_grad(exponent, genomic_dis=genomic_dis, freq_smooth=freq_smooth, scaling_factor=scaling_factor)).ravel()
+
+
+def estimate_exponent(genomic_dis, freq_smooth, x0=None, bounds=(-10, 10), seed=0,
+                      max_iter=1e20, verbose=True):
+    # Define initial value (x0) and bounds on X
+    if x0 is None:
+        rng = np.random.default_rng(seed=seed)
+        x0 = rng.uniform(low=bounds[0], high=bounds[1])
+    x0 = np.array(x0, ndmin=1, copy=None)
+    bounds = np.array(bounds, ndmin=2, copy=None).reshape(1, -1)
+
+    # Build args
+    args = [genomic_dis, freq_smooth]
+
+    # Optimize
+    if verbose:
+        print("OPTIMIZING:", flush=True)
+    obj = obj_wrap(x0, *args)
+    if max_iter == 0:
+        X = x0
+        d = {'converged': 'N/A', 'obj': obj._value}
+    else:
+        results = optimize.fmin_l_bfgs_b(
+            obj_wrap, x0=x0, fprime=grad_wrap, bounds=bounds, args=args,
+            factr=1e-20, maxls=40, pgtol=1e-10, maxiter=int(max_iter)) #, maxfun=max_fun)
+        X, obj, d = results
+        d['converged'] = d['warnflag'] == 0
+        conv_desc = d['task']
+        d['obj'] = obj
+    if X.size == 1:
+        d['exponent'] = X[0]
+    else:
+        d['exponent'] = X
+    d['scaling_factor'] = float(estimate_scaling_factor(X, *args)._value)
+
+    if verbose:
+        print("\nRESULTS:", flush=True)
+        print_alpha_infer_results(d)
+
+    return d
--- jupyter/fit_pseudocount_func.py
+++ jupyter/fit_pseudocount_func.py
@@ -28,6 +28,260 @@ from jax.nn import relu
 from scipy import optimize
 
 
+from pastis.optimization.utils_poisson import _setup_jax
+_setup_jax(traceback=False, debug_nan_inf=False)
+
+from scipy import optimize
+import jax.numpy as jnp
+from jax import grad, jit
+from jax.nn import relu
+
+
+def prep_data_for_inference(dir_matrix2d, mcool_file, lengths_file=None, resolution=2.5e6, verbose=True):
+    matrix_df, sc_dis, lengths_df = load(dir_matrix2d=dir_matrix2d, mcool_file=mcool_file, lengths_file=lengths_file, resolution=resolution, verbose=verbose)
+
+    # Setup data for inference
+    _, snm3c, dis_idx = ambiguate_matrix_df_for_inference(matrix_df)
+    sc_dis_arr = sc_dis.loc[dis_idx].values
+    sc_dis_arr[np.isnan(sc_dis_arr)] = 0
+    sc_dis_arr = np.asarray(sc_dis_arr, order='C')
+    data = InferArgs(snm3c=snm3c, sc_dis_arr=sc_dis_arr)
+
+    return data
+
+# ===================================================================================================================
+# ===================================================================================================================
+# ===================================================================================================================
+
+def logistic_jax(d, X, infer_nu=False, infer_q=False):
+    q = nu = 1
+    if infer_nu and infer_q:
+        k, d0, nu, q = X
+    elif infer_nu:
+        k, d0, nu = X
+    elif infer_q:
+        k, d0, q = X
+    else:
+        k, d0 = X
+
+    tmp = -k * (d - d0)
+    bar = 1 + q * jnp.exp(tmp)
+    baz = jnp.power(relu(bar), 1 / nu)
+    counts = 1 / baz
+
+    # counts = 1 / jnp.power(relu(1 + q * jnp.exp(tmp)), 1 / nu)
+    # counts = 1 / jnp.power(1 + q * jnp.exp(tmp), 1 / nu)
+    return counts
+
+
+def get_pseudocounts_scaling_factor(pseudocounts, snm3c):
+    scaling_factor = jnp.sum(pseudocounts * snm3c) / jnp.sum(jnp.square(pseudocounts))
+    return scaling_factor
+
+
+def get_bulk_pseudocounts(X, data, intramol_only=False, infer_nu=False,
+                          infer_q=False):
+    nbins = data.snm3c.size
+
+    if intramol_only:
+        dis_sc = data.dis[:nbins * 2]
+    else:
+        dis_sc = data.dis
+    
+    mask_sc = dis_sc > 0
+    counts_sc = jnp.where(
+        mask_sc,
+        logistic_jax(dis_sc, X, infer_nu=infer_nu, infer_q=infer_q),
+        jnp.zeros_like(dis_sc))
+    mask_bulk = mask_sc.sum(axis=1)
+    counts = jnp.where(
+        mask_bulk > 0, counts_sc.sum(axis=1) / mask_bulk,
+        jnp.zeros(counts_sc.shape[0])).reshape(-1, nbins).sum(axis=0)
+
+    return counts
+
+
+def obj_eval_pseudocounts(X, data, intramol_only=False, infer_nu=False,
+                          infer_q=False, obj_type='pearson', verbose=False):
+    if verbose:
+        if type(X).__name__ == "DynamicJaxprTracer":
+            print("Compiling objective function via Jax JIT...", flush=True)
+        elif type(X).__name__ == "JVPTracer":
+            print("Compiling gradient function via Jax JIT...", flush=True)
+
+    if isinstance(obj_type, str):
+        obj_type = (obj_type.lower(),)
+    else:
+        obj_type = tuple([x.lower() for x in obj_type])
+
+    counts = get_bulk_pseudocounts(
+        X, data=data, intramol_only=intramol_only, infer_nu=infer_nu,
+        infer_q=infer_q)
+
+    obj = 0
+    if 'pearson' in obj_type:
+        pearson_r = jnp.corrcoef(data.snm3c, counts)[0, 1]
+        obj += -pearson_r
+    if ('rmse' in obj_type) or ('mse' in obj_type):
+        scaling_factor = get_pseudocounts_scaling_factor(counts, snm3c=data.snm3c)
+        mse = jnp.mean(jnp.square(counts - data.snm3c))
+        if 'mse' in obj_type:
+            obj += mse
+        elif 'rmse' in obj_type:
+            obj += jnp.sqrt(mse)
+        else:
+            raise ValueError(f"{obj_type=}")
+    else:
+        raise ValueError(f"{obj_type=}")
+
+    return obj
+
+
+grad_eval_pseudocounts = grad(obj_eval_pseudocounts)
+obj_eval_pseudocounts_jit = jit(
+    obj_eval_pseudocounts, static_argnames=['data', 'intramol_only', 'infer_nu', 'infer_q', 'obj_type'])
+grad_eval_pseudocounts_jit = grad(obj_eval_pseudocounts_jit)
+
+
+def obj_wrap(X, data, intramol_only=False, infer_nu=False, infer_q=False,
+             obj_type='pearson', jitted=False, verbose=False):
+    if jitted:
+        objective_func = obj_eval_pseudocounts_jit
+    else:
+        objective_func = obj_eval_pseudocounts
+    
+    obj = objective_func(
+        X, data=data, intramol_only=intramol_only, infer_nu=infer_nu,
+        infer_q=infer_q, obj_type=obj_type)
+    if not isinstance(obj, float) and obj.size > 1:
+        obj = sum(obj)
+    if verbose:
+        print_iter(X, infer_nu=infer_nu, infer_q=infer_q, note=obj)
+    return obj
+
+
+def grad_wrap(X, data, intramol_only=False, infer_nu=False, infer_q=False,
+              obj_type='pearson', jitted=False, verbose=False):
+    if jitted:
+        gradient_func = grad_eval_pseudocounts_jit
+    else:
+        gradient_func = grad_eval_pseudocounts
+    
+    return np.array(gradient_func(
+        X, data=data, intramol_only=intramol_only, infer_nu=infer_nu,
+        infer_q=infer_q, obj_type=obj_type)).ravel()
+
+
+def estimate_logistic_param(data, intramol_only=False, infer_nu=False, infer_q=False,
+                            obj_type='pearson', init=None, bounds=None, seed=0, max_iter=1e20,
+                            factr=1e7, maxls=20, pgtol=1e-05,
+                            jitted=False, verbose=True):
+
+    if isinstance(obj_type, str):
+        obj_type = (obj_type.lower(),)
+    else:
+        obj_type = tuple([x.lower() for x in obj_type])
+
+    # Get bounds
+    buffer = 0.1
+    if bounds is None:
+        bounds = np.array([
+            [-np.inf, 0 - buffer],  # k < 0
+            [0, 1]])  # 0 <= d0 <= 1
+        if infer_nu:  # 0 < nu <= 1
+            bounds = np.concatenate(bounds, np.array([[0 + buffer, 1]]))
+        if infer_q:  # q > 0
+            bounds = np.concatenate(bounds, np.array([[0 + buffer, np.inf]]))
+        bounds = np.nan_to_num(bounds, posinf=50, neginf=-50)  # XXX
+    else:
+        bounds = np.array(bounds, ndmin=2, copy=None).reshape(-1, 2)
+    
+    # Define initial value for X
+    if init is None:
+        rng = np.random.default_rng(seed=seed)
+        tmp = np.nan_to_num(bounds, posinf=20, neginf=-20)
+        init = rng.uniform(low=tmp[:, 0], high=tmp[:, 1])
+        if verbose:
+            print_iter(X=init, infer_nu=infer_nu, infer_q=infer_q, note='INIT')
+    init = np.array(init, ndmin=1, copy=None)
+
+    # Optimize
+    args = [data, intramol_only, infer_nu, infer_q, obj_type, jitted, verbose > 1]
+    if verbose:
+        print("OPTIMIZING:", flush=True)
+    obj = obj_wrap(
+        init, data=data, intramol_only=intramol_only, infer_nu=infer_nu, infer_q=infer_q,
+        obj_type=obj_type, jitted=False, verbose=verbose > 1)
+    if max_iter == 0:
+        X = init
+        d = {'converged': 'N/A', 'obj': obj._value}
+    else:
+        results = optimize.fmin_l_bfgs_b(
+            obj_wrap, x0=init, fprime=grad_wrap, bounds=bounds, args=args,
+            factr=factr, maxls=maxls, pgtol=pgtol, maxiter=int(max_iter)) #, maxfun=max_fun)
+        X, obj, d = results
+        d['converged'] = d['warnflag'] == 0
+        conv_desc = d['task']
+        d['obj'] = obj
+    if X.size == 1:
+        d['params'] = X[0]
+    else:
+        d['params'] = X
+    counts = get_bulk_pseudocounts(
+        X, data=data, intramol_only=intramol_only, infer_nu=infer_nu,
+        infer_q=infer_q)
+    d['scaling_factor'] = float(get_pseudocounts_scaling_factor(counts, snm3c=data.snm3c)._value)
+
+    if verbose:
+        print("\nRESULTS:", flush=True)
+        print_infer_results_pseudocounts(d, infer_nu=infer_nu, infer_q=infer_q)
+
+    return d
+
+# ===================================================================================================================
+
+def print_iter(X, infer_nu=False, infer_q=False, note=None):
+    q = nu = 1
+    if infer_nu and infer_q:
+        k, d0, nu, q = X
+    elif infer_nu:
+        k, d0, nu = X
+    elif infer_q:
+        k, d0, q = X
+    else:
+        k, d0 = X
+
+    to_print = f"k={k:<8.6g}  d₀={d0:<8.6g}"
+    if infer_nu:
+        to_print += f"  𝜈={nu:<8.6g}"
+    if infer_q:
+        to_print += f"  q={q:<8.6g}"
+    if isinstance(note, (str, int)):
+        to_print += f" ... {note}"
+    elif note is not None:
+        to_print += f" ... obj={note:.8g}"
+    print(to_print, flush=True)
+
+
+def print_infer_results_pseudocounts(d, infer_nu=False, infer_q=False):
+    for k, v in d.items():
+        if k == 'params':
+            v = np.array(v, ndmin=1, copy=None)
+            print_iter(v, infer_nu=infer_nu, infer_q=infer_q)
+            continue
+
+        if k == 'grad':
+            v = f"{v.mean():.3g}"
+        elif isinstance(v, float):
+            v = f"{v:.3g}"
+        elif isinstance(v, np.ndarray) and v.size > 1:
+            v = ', '.join([f"{x:.3g}" for x in v])
+        print(f"{k.ljust(10)}   {v}", flush=True)
+
+# ===================================================================================================================
+# ===================================================================================================================
+# ===================================================================================================================
+
 class InferArgs(object):
     def __init__(self, snm3c, sc_dis_arr):
         self.snm3c = snm3c
@@ -78,60 +332,63 @@ def ambiguate_matrix_df_for_inference(matrix_df):
     return matrix_df_ambig, snm3c, dis_idx
 
 
-def load(dir_matrix2d, mcool_file, lengths_file, resolution=2.5e6, verbose=True):
+def load(dir_matrix2d, mcool_file, lengths_file=None, resolution=2.5e6, verbose=True):
+    if lengths_file is None:
+        lengths_file = os.path.join(os.path.dirname(dir_matrix2d), 'counts.bed')
+
     # Get single-cell distances: intra-molecular
     sc_dis_intramol_file = glob.glob(os.path.join(dir_matrix2d, '*.distances.intramol.tsv.gz'))
     if len(sc_dis_intramol_file) != 1:
         raise ValueError("Couldn't find unique file for intra-mol single cell distances")
     sc_dis_intramol_file = sc_dis_intramol_file[0]
-    
+
     sc_dis = pd.read_csv(
         sc_dis_intramol_file, sep='\t', header=None, index_col=0,
         converters={0: ast.literal_eval})
     sc_dis.index.name = None
-    
+
     # Get single-cell distances: intra-chromosomal, inter-homolog
     sc_dis_diffH_file = glob.glob(os.path.join(dir_matrix2d, '*.distances.sameC-diffH.tsv.gz'))
     if len(sc_dis_diffH_file) != 1:
         raise ValueError("Couldn't find unique file for intra-chrom, inter-hmlg single cell distances")
     sc_dis_diffH_file = sc_dis_diffH_file[0]
-    
+
     sc_dis_diffH = pd.read_csv(
         sc_dis_diffH_file, sep='\t', header=None, index_col=0,
         converters={0: ast.literal_eval})
     sc_dis_diffH.index.name = None
-    
+
     # Single-cell distances: combine intra-chromosomal data
     sc_dis = pd.concat([sc_dis, sc_dis_diffH])
-    
+
     # Load snm3c-seq data via cooler
     resolution = int(resolution)
     uri = f'{mcool_file}::/resolutions/{resolution:d}'
     clr = cooler.Cooler(uri)
     snm3c = np.triu(clr.matrix(balance=True if 'Raw.' in mcool_file else False)[:, :], 1)
-    
+
     clr_bins = clr.bins()[:].reset_index().rename({'index': 'idx_clr'}, axis=1)
     clr_bins['mid'] = clr_bins[['start', 'end']].mean(axis=1).round().astype(int)
     assert len(clr_bins) == snm3c.shape[0]
-    print(snm3c.shape)
-    
+    # print(snm3c.shape)
+
     nbins_clr = clr_bins.groupby('chrom', observed=True).size().sort_values(
         ascending=False).drop('chrX')
     lengths_clr = clr_bins.groupby('chrom', observed=True).apply(
         lambda x: int(round(x.end.max() / resolution)), include_groups=False).drop('chrX')
     assert (np.round((clr.chromsizes / resolution)).drop('chrX') - lengths_clr == 0).all()
-    
-    
+
+
     # Setup chromosome lengths data
     lengths_df = pd.read_csv(lengths_file, sep='\t')
     lengths_df.columns = [c.strip('#') for c in lengths_df.columns]
     for col in ['start', 'end', 'mid']:  # Round to nearest 10,000bp
         lengths_df[col] = lengths_df[col].round(-4)
-    
+
     n = len(lengths_df)
     lengths_s = lengths_df.groupby('chrom').size().sort_values(
         ascending=False)
-    
+
     lengths_compare = pd.concat([lengths_s.to_frame().rename({0: 'merfish'}, axis=1), lengths_clr.to_frame().rename({0: 'snm3c'}, axis=1)], axis=1)
     lengths_compare['difference'] = lengths_compare.snm3c - lengths_compare.merfish
     assert (lengths_compare.difference >= 0).all()
@@ -140,26 +397,23 @@ def load(dir_matrix2d, mcool_file, lengths_file, resolution=2.5e6, verbose=True)
         print(f"{lengths_df.idx_clr.isnull().sum()} genomic bins are present in MERFISH but not snm3c", flush=True)
     lengths_df.index = lengths_df.idx_genome
     lengths_df.index.name = None
-    
-    # Setup matrix-related data
+
+    # Setup matrix-related data for intra-chromosomal (withinter- and intra-homolog)
     matrix_df = make_matrix_df(lengths_df)
     matrix_df = matrix_df[matrix_df['mask.sameC-sameH'] | matrix_df['mask.sameC-diffH']].drop(
         ['mask.sameC-sameH', 'mask.sameC-diffH', 'mask.diffC-sameH', 'mask.diffC-diffH', 'j.chrom'], axis=1).rename(
         {'i.chrom': 'chrom'}, axis=1)
     matrix_df['i.idx_clr'] = lengths_df.loc[matrix_df['i.idx_ambig'], 'idx_clr'].values.ravel()
     matrix_df['j.idx_clr'] = lengths_df.loc[matrix_df['j.idx_ambig'], 'idx_clr'].values
-    
+
     mask = matrix_df['i.idx_clr'].notnull() & matrix_df['j.idx_clr'].notnull()
     matrix_df.loc[mask, 'snm3c'] = snm3c[matrix_df.loc[mask, 'i.idx_clr'].astype(int), matrix_df.loc[mask, 'j.idx_clr'].astype(int)]
     matrix_df.loc[matrix_df['i.idx_ambig'] == matrix_df['j.idx_ambig'], 'snm3c'] = np.nan
-    
+
     # Filter for locus pairs present in single-cell distances
     matrix_df = matrix_df.loc[matrix_df.index.isin(sc_dis.index)]
 
+    # Genomic distances, including for inter-homolog
+    matrix_df['genomic_dis_ambig'] = (matrix_df['i.idx_ambig'] - matrix_df['j.idx_ambig']).abs()
+
     return matrix_df, sc_dis, lengths_df
-
-
-# ===================================================================================================================
-# ===================================================================================================================
-
-
